f1230b897a5b05eb058bdfe52df65e8fec871415
kudo <kichikchimail@gmail.com>
2020-05-19 11:16:54 +0900

    kadai5

--- src/main/java/jp/te4a/spring/boot/myapp4/HelloContoller.java
+++ src/main/java/jp/te4a/spring/boot/myapp4/HelloContoller.java
@@ -1,12 +1,13 @@
 package jp.te4a.spring.boot.myapp4;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloContoller {
 	@RequestMapping("/")
 	public String index() {
-		return "this is Spring Boot sample";
+		return "index";
 	}
 }
