07d4bfc8f025b5d2bd8c9aea0f1846b818211fc2
kudo <kichikchimail@gmail.com>
2020-05-20 10:43:33 +0900

    kadai5 finish

--- src/main/java/jp/te4a/spring/boot/myapp4/HelloContoller.java
+++ src/main/java/jp/te4a/spring/boot/myapp4/HelloContoller.java
@@ -1,13 +1,24 @@
 package jp.te4a.spring.boot.myapp4;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloContoller {
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("msg","this is asetting message");
 		return "index";
+	}
+	
+	@RequestMapping(value="/post",method=RequestMethod.POST)
+	public ModelAndView postForm(@RequestParam("text1")String text1) {
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("msg", "you write '" + text1 + "'!!!");
+		return mv;
 	}
 }
